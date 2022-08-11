952bcee4194b67b28e16330c884f1c2b87bc190f
ITCMARA <mara@itcfyn.dk>
2022-08-11 08:24:40 +0200

    Initial commit

---
+++ Capa.PowerShell.Module.psm1
@@ -0,0 +1,20 @@
+﻿<#	
+	===========================================================================
+	 Created with: 	SAPIEN Technologies, Inc., PowerShell Studio 2022 v5.8.209
+	 Created on:   	11-08-2022 08:22
+	 Created by:   	Mark5900
+	 Organization: 	
+	 Filename:     	Capa.PowerShell.Module.psm1
+	-------------------------------------------------------------------------
+	 Module Name: Capa.PowerShell.Module
+	===========================================================================
+#>
+
+
+
+function Write-HelloWorld
+{
+	Write-Host "Hello World"
+}
+
+
---
+++ Test-Module.ps1
@@ -0,0 +1,37 @@
+﻿<#	
+	.NOTES
+	===========================================================================
+	 Created with: 	SAPIEN Technologies, Inc., PowerShell Studio 2022 v5.8.209
+	 Created on:   	11-08-2022 08:22
+	 Created by:   	Mark5900
+	 Organization: 	
+	 Filename:     	Test-Module.ps1
+	===========================================================================
+	.DESCRIPTION
+	The Test-Module.ps1 script lets you test the functions and other features of
+	your module in your PowerShell Studio module project. It's part of your project,
+	but it is not included in your module.
+
+	In this test script, import the module (be careful to import the correct version)
+	and write commands that test the module features. You can include Pester
+	tests, too.
+
+	To run the script, click Run or Run in Console. Or, when working on any file
+	in the project, click Home\Run or Home\Run in Console, or in the Project pane, 
+	right-click the project name, and then click Run Project.
+#>
+
+
+#Explicitly import the module for testing
+Import-Module 'Capa.PowerShell.Module'
+
+#Run each module function
+Write-HelloWorld
+
+#Sample Pester Test
+#Describe "Test Capa.PowerShell.Module" {
+#	It "tests Write-HellowWorld" {
+#		Write-HelloWorld | Should BeExactly "Hello World"
+#	}	
+#}
+

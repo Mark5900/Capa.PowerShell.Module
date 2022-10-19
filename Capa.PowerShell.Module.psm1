b38d501022ea404d316d13747f4bd77275aa1bef
ITCMARA <mara@itcfyn.dk>
2022-10-19 13:27:30 +0200

    Tilføjede Remove-CapaUnitFromGroup

--- Capa.PowerShell.Module.psm1
+++ Capa.PowerShell.Module.psm1
@@ -1605,3 +1605,54 @@ function Get-CapaUnitFolder
 	
 	Return $bool
 }
+
+<#
+	.SYNOPSIS
+		https://capasystems.atlassian.net/wiki/spaces/CI64DOC/pages/19306247664/Remove+unit+from+group
+	
+	.DESCRIPTION
+		A detailed description of the Remove-CapaUnitFromGroup function.
+	
+	.PARAMETER CapaSDK
+		A description of the CapaSDK parameter.
+	
+	.PARAMETER UnitName
+		A description of the UnitName parameter.
+	
+	.PARAMETER UnitType
+		A description of the UnitType parameter.
+	
+	.PARAMETER GroupName
+		A description of the GroupName parameter.
+	
+	.PARAMETER GroupType
+		A description of the GroupType parameter.
+	
+	.EXAMPLE
+		PS C:\> Remove-CapaUnitFromGroup -CapaSDK $value1 -UnitName 'Value2' -UnitType Computer -GroupName 'Value4' -GroupType ""
+	
+	.NOTES
+		Additional information about the function.
+#>
+function Remove-CapaUnitFromGroup
+{
+	param
+	(
+		[Parameter(Mandatory = $true)]
+		$CapaSDK,
+		[Parameter(Mandatory = $true)]
+		[string]$UnitName,
+		[Parameter(Mandatory = $true)]
+		[ValidateSet('Computer', 'User')]
+		[string]$UnitType,
+		[Parameter(Mandatory = $true)]
+		[string]$GroupName,
+		[Parameter(Mandatory = $true)]
+		[ValidateSet('Dynamic_ADSI', 'Calendar', 'Department', 'Dynamic_SQL', 'Reinstall', 'Security', 'Static')]
+		[string]$GroupType
+	)
+	
+	$bool = $CapaSDK.RemoveUnitFromGroup($UnitName, $UnitType, $GroupName, $GroupType)
+	
+	Return $bool
+}

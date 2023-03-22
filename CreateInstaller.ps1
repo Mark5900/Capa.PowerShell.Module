d55379bc899c6248e80824c542a457a55a0ee259
Mark5900 <56847489+Mark5900@users.noreply.github.com>
2023-03-22 20:45:30 +0100

    Updated documentation and new installer

---
+++ CreateInstaller.ps1
@@ -0,0 +1,75 @@
+Import-Module PSMSI
+$ProductName = 'Capa.PowerShell.Module'
+$UpgradeCode = '84859CA1-0F7D-47BF-8D36-AE22F5E171AD'
+$Version = '1.0.18.0'
+
+
+New-Installer -ProductName $ProductName -UpgradeCode $UpgradeCode -Content {
+    New-InstallerDirectory -PredefinedDirectoryName ProgramFiles64Folder -Content {
+        New-InstallerDirectory -DirectoryName 'PowerShell' -Content {
+            New-InstallerDirectory -DirectoryName 'Modules' -Content {
+                New-InstallerDirectory -DirectoryName 'Capa.PowerShell.Module' -Content {
+                    New-InstallerFile -Source .\Modules\Capa.PowerShell.Module\Capa.PowerShell.Module.psd1
+                }
+                New-InstallerDirectory -DirectoryName 'Capa.PowerShell.Module.SDK' -Content {
+                    New-InstallerFile -Source .\Modules\Capa.PowerShell.Module.SDK\Capa.PowerShell.Module.SDK.psd1
+                }
+                New-InstallerDirectory -DirectoryName 'Capa.PowerShell.Module.SDK.Authentication' -Content {
+                    New-InstallerFile -Source .\Modules\Capa.PowerShell.Module.SDK.Authentication\Capa.PowerShell.Module.SDK.Authentication.psd1
+                    New-InstallerFile -Source .\Modules\Capa.PowerShell.Module.SDK.Authentication\Capa.PowerShell.Module.SDK.Authentication.psm1
+                }
+                New-InstallerDirectory -DirectoryName 'Capa.PowerShell.Module.SDK.Container' -Content {
+                    New-InstallerFile -Source .\Modules\Capa.PowerShell.Module.SDK.Container\Capa.PowerShell.Module.SDK.Container.psd1
+                    New-InstallerFile -Source .\Modules\Capa.PowerShell.Module.SDK.Container\Capa.PowerShell.Module.SDK.Container.psm1
+                }
+                New-InstallerDirectory -DirectoryName 'Capa.PowerShell.Module.SDK.Group' -Content {
+                    New-InstallerFile -Source .\Modules\Capa.PowerShell.Module.SDK.Group\Capa.PowerShell.Module.SDK.Group.psd1
+                    New-InstallerFile -Source .\Modules\Capa.PowerShell.Module.SDK.Group\Capa.PowerShell.Module.SDK.Group.psm1
+                }
+                New-InstallerDirectory -DirectoryName 'Capa.PowerShell.Module.SDK.Inventory' -Content {
+                    New-InstallerFile -Source .\Modules\Capa.PowerShell.Module.SDK.Inventory\Capa.PowerShell.Module.SDK.Inventory.psd1
+                    New-InstallerFile -Source .\Modules\Capa.PowerShell.Module.SDK.Inventory\Capa.PowerShell.Module.SDK.Inventory.psm1
+                }
+                New-InstallerDirectory -DirectoryName 'Capa.PowerShell.Module.SDK.MDM' -Content {
+                    New-InstallerFile -Source .\Modules\Capa.PowerShell.Module.SDK.MDM\Capa.PowerShell.Module.SDK.MDM.psd1
+                    New-InstallerFile -Source .\Modules\Capa.PowerShell.Module.SDK.MDM\Capa.PowerShell.Module.SDK.MDM.psm1
+                }
+                New-InstallerDirectory -DirectoryName 'Capa.PowerShell.Module.SDK.OSDeployment' -Content {
+                    New-InstallerFile -Source .\Modules\Capa.PowerShell.Module.SDK.OSDeployment\Capa.PowerShell.Module.SDK.OSDeployment.psd1
+                    New-InstallerFile -Source .\Modules\Capa.PowerShell.Module.SDK.OSDeployment\Capa.PowerShell.Module.SDK.OSDeployment.psm1
+                }
+                New-InstallerDirectory -DirectoryName 'Capa.PowerShell.Module.SDK.Package' -Content {
+                    New-InstallerFile -Source .\Modules\Capa.PowerShell.Module.SDK.Package\Capa.PowerShell.Module.SDK.Package.psd1
+                    New-InstallerFile -Source .\Modules\Capa.PowerShell.Module.SDK.Package\Capa.PowerShell.Module.SDK.Package.psm1
+                }
+                New-InstallerDirectory -DirectoryName 'Capa.PowerShell.Module.SDK.SystemSdk' -Content {
+                    New-InstallerFile -Source .\Modules\Capa.PowerShell.Module.SDK.SystemSdk\Capa.PowerShell.Module.SDK.SystemSdk.psd1
+                    New-InstallerFile -Source .\Modules\Capa.PowerShell.Module.SDK.SystemSdk\Capa.PowerShell.Module.SDK.SystemSdk.psm1
+                }
+                New-InstallerDirectory -DirectoryName 'Capa.PowerShell.Module.SDK.Unit' -Content {
+                    New-InstallerFile -Source .\Modules\Capa.PowerShell.Module.SDK.Unit\Capa.PowerShell.Module.SDK.Unit.psd1
+                    New-InstallerFile -Source .\Modules\Capa.PowerShell.Module.SDK.Unit\Capa.PowerShell.Module.SDK.Unit.psm1
+                }
+                New-InstallerDirectory -DirectoryName 'Capa.PowerShell.Module.SDK.User' -Content {
+                    New-InstallerFile -Source .\Modules\Capa.PowerShell.Module.SDK.User\Capa.PowerShell.Module.SDK.User.psd1
+                    New-InstallerFile -Source .\Modules\Capa.PowerShell.Module.SDK.User\Capa.PowerShell.Module.SDK.User.psm1
+                }
+                New-InstallerDirectory -DirectoryName 'Capa.PowerShell.Module.SDK.Utilities' -Content {
+                    New-InstallerFile -Source .\Modules\Capa.PowerShell.Module.SDK.Utilities\Capa.PowerShell.Module.SDK.Utilities.psd1
+                    New-InstallerFile -Source .\Modules\Capa.PowerShell.Module.SDK.Utilities\Capa.PowerShell.Module.SDK.Utilities.psm1
+                }
+                New-InstallerDirectory -DirectoryName 'Capa.PowerShell.Module.SDK.VPP' -Content {
+                    New-InstallerFile -Source .\Modules\Capa.PowerShell.Module.SDK.VPP\Capa.PowerShell.Module.SDK.VPP.psd1
+                    New-InstallerFile -Source .\Modules\Capa.PowerShell.Module.SDK.VPP\Capa.PowerShell.Module.SDK.VPP.psm1
+                }
+                New-InstallerDirectory -DirectoryName 'Capa.PowerShell.Module.SDK.WSUS' -Content {
+                    New-InstallerFile -Source .\Modules\Capa.PowerShell.Module.SDK.WSUS\Capa.PowerShell.Module.SDK.WSUS.psd1
+                    New-InstallerFile -Source .\Modules\Capa.PowerShell.Module.SDK.WSUS\Capa.PowerShell.Module.SDK.WSUS.psm1
+                }
+            }
+        } 
+    }
+}-OutputDirectory (Join-Path $PSScriptRoot 'Installers') -RequiresElevation -Version $Version -Manufacturer 'Mark5900' -Platform x64
+
+Remove-Item ".\Installers\Capa.PowerShell.Module.$Version.x64.wxs" -Force
+Remove-Item ".\Installers\Capa.PowerShell.Module.$Version.x64.wxsobj" -Force

5eeb80e35a0c88373e56b0a62142aab1b3b2c287
Mark5900 <56847489+Mark5900@users.noreply.github.com>
2023-04-23 15:59:56 +0200

    Update CreateInstaller.ps1
    Auto generate function documentation

--- CreateInstaller.ps1
+++ CreateInstaller.ps1
@@ -350,10 +350,249 @@ function New-ModuleInstallerPowerPackOnly {
     Remove-Item "$PSScriptRoot\Installers\$ProductName.$Version.x64.wxsobj" -Force
 }
 
+function GenerateSyntaxText {
+    param (
+        $GetHelp
+    )
+    $Text = ''
+
+    foreach ($Syntax in $GetHelp.Syntax.syntaxItem) {
+        $Text += '```powershell'
+        $Text += "`n"
+        
+        $Text += "$($Syntax.Name)`n"
+        foreach ($Parameter in $Syntax.Parameter) {
+            $Text += "`t-"
+            
+            if ($Parameter.Required) {
+                $Text += "$($Parameter.Name)"
+            } else {
+                $Text += "[$($Parameter.Name)]"
+            }
+
+            $Text += " <$($Parameter.parameterValue)>`n"
+        }
+
+
+        $Text += '```'
+        $Text += "`n"
+    }
+
+    return $Text
+}
+
+function GenerateExaplesText {
+    param (
+        $GetHelp
+    )
+    $Text = ''
+    $Number = 1
+
+    foreach ($Example in $GetHelp.Examples.example) {
+        $Text += "### Example $Number"
+        $Text += "`n"
+        
+        $Text += '```powershell'
+        $Text += "`n"
+
+        $Text += "$($Example.code)`n"
+
+        $Text += '```'
+        $Text += "`n"
+
+        if ($null -ne $Example.remarks -and $Example.remarks -ne '') {
+            $Text += "$($Example.remarks)`n"
+        }
+
+        $Number++
+    }
+
+    return $Text
+}
+
+function Get-ValidateSet {
+    param(
+        $FunctionPath
+    )
+    $ValidateSet = @()
+    $Values = $null
+    $FileContent = Get-Content $FunctionPath
+
+    foreach ($Line in $FileContent) {
+        if ($Line -like '*ValidateSet*') {
+            $Values += $Line
+            $ParameterName = $null
+        }
+
+        if ($Line -like '*$*' -and $Line -notlike '*Mandatory*') {
+            $ParameterName = $Line
+        }
+
+        if ($null -ne $Values -and $null -ne $ParameterName) {
+            $ValidateSetValue = $null
+
+            $Values = $Values.Split(',')
+            foreach ($Value in $Values) {
+                $text = GetStringBetweenTwoStrings -Content $Value -firstString "'" -secondString "'"
+
+                if ($null -ne $ValidateSetValue) {
+                    $ValidateSetValue += ", $text"
+                } else {
+                    $ValidateSetValue = $text
+                }
+            }
+
+            $ParameterName = $ParameterName.Split('=')[0].Trim()
+            $ParameterName = $ParameterName.Split('$')[1].Trim()
+
+            $ValidateSet += @{ Name = $ParameterName; Values = $ValidateSetValue }
+
+            $Values = $null
+        }
+    }
+    
+    return $ValidateSet
+}
+
+function GenerateParametersText {
+    param (
+        $GetHelp,
+        $FunctionPath
+    )
+    $Text = ''
+    $ValidateSet = Get-ValidateSet -FunctionPath $FunctionPath
+
+    foreach ($Parameter in $GetHelp.Parameters.parameter) {
+        $Text += "-**$($Parameter.name)**`n`n"
+        $Text += "$($Parameter.description.Text)`n"
+        $Text += "| Name | Value |`n"
+        $Text += "| ---- | ---- |`n"
+        $Text += "| Type: | $($Parameter.type.name) |`n"
+
+        # Accepted values: for parameters
+        # FIXME - this is not working for all parameters but some of them
+        if ($Validate.Name -contains $Parameter.name) {
+            Write-Host "Found validate set for $($Parameter.name)"
+            $Value = $ValidateSet | Where-Object { $_.Name -eq $Parameter.name }
+            Write-Host "Values: $($Value.Values)"
+            $Text += "| Accepted values: | $($Value.Values) | `n"
+        }
+
+        $Text += "| Position: | $($Parameter.position) | `n"
+
+        if ($null -eq $Parameter.defaultValue -or $Parameter.defaultValue -eq '') {
+            $Value = 'None'
+        } else {
+            $Value = $Parameter.defaultValue
+        }
+        $Text += "| Default value: | $Value | `n"
+
+        $Text += "| Accept pipeline input: | $($Parameter.pipelineInput) | `n"
+        $Text += "| Accept wildcard characters: | $($Parameter.globbing) | `n`n"
+    }
+
+    return $Text
+}
+
+function GetStringBetweenTwoStrings {
+    param (
+        $importPath,
+        $firstString,
+        $secondString,
+        $Content = $null
+    )
+
+    #Get content from file
+    if ($null -eq $Content) {
+        $file = Get-Content $importPath
+    } else {
+        $file = $Content
+    }
+
+    #Regex pattern to compare two strings
+    $pattern = "$firstString(.*?)$secondString"
+
+    #Perform the opperation
+    $result = [regex]::Match($file, $pattern).Groups[1].Value
+
+    #Return result
+    return $result
+
+}
+
+function GenerateFunctionsDocumentation {
+    $DocumentationFolder = Join-Path $PSScriptRoot 'Documentation' 'Functions'
+    $ModulePath = Join-Path $PSScriptRoot 'Modules'
+
+    # Add overview page for all functions in documentation folder
+    $OverviewFile = Join-Path $PSScriptRoot 'Documentation' 'Overview of all functions in modules.md'
+    Out-File -FilePath $OverviewFile -InputObject '# Overview of all functions in modules' -Force
+
+    # Delete all files in the folder 
+    Get-ChildItem -Path $DocumentationFolder -Recurse | Remove-Item -Force
+
+    # Generate documentation for all functions in the modules
+    $Folders = Get-ChildItem -Path $ModulePath -Directory
+    foreach ($Folder in $Folders) {
+        $DevFolder = Join-Path $Folder.FullName 'Dev'
+
+        if (Test-Path $DevFolder) {
+            $Files = Get-ChildItem -Path $DevFolder -File | Where-Object { $_.Name -notlike '*Tests.ps1' }
+
+            # Add overview page for all functions in module
+            Out-File -FilePath $OverviewFile -InputObject "`n## $($Folder.Name)`n" -Append
+
+            foreach ($File in $Files) {
+                Write-Host "Generating documentation for $($File.Name)"
+
+                $FunctionName = $File.Name -replace '.ps1'
+                $DocumentationFile = Join-Path $DocumentationFolder "$FunctionName.md"
+
+                Import-Module $File.FullName -Force
+                $GetHelp = Get-Help $FunctionName -Full
+
+                # Title
+                Out-File -FilePath $DocumentationFile -InputObject "# $($GetHelp.Name)" -Append
+                
+                # Module
+                Out-File -FilePath $DocumentationFile -InputObject "Module: $($Folder.Name)`n" -Append
+
+                # Synopsis
+                Out-File -FilePath $DocumentationFile -InputObject "$($GetHelp.Synopsis)`n" -Append
+
+                # Syntax
+                Out-File -FilePath $DocumentationFile -InputObject "## Syntax`n" -Append
+                Out-File -FilePath $DocumentationFile -InputObject (GenerateSyntaxText -GetHelp $GetHelp) -Append
+
+                # Description
+                Out-File -FilePath $DocumentationFile -InputObject "## Description`n" -Append
+                Out-File -FilePath $DocumentationFile -InputObject "$($GetHelp.Description.Text)`n" -Append
+
+                # Examples
+                Out-File -FilePath $DocumentationFile -InputObject "## Examples`n" -Append
+                Out-File -FilePath $DocumentationFile -InputObject (GenerateExaplesText -GetHelp $GetHelp) -Append
+
+                # Parameters
+                Out-File -FilePath $DocumentationFile -InputObject "## Parameters`n" -Append
+                $Text = GenerateParametersText -GetHelp $GetHelp -FunctionPath $File.FullName
+                Out-File -FilePath $DocumentationFile -InputObject ($Text) -Append
+
+                # Notes
+                Out-File -FilePath $DocumentationFile -InputObject "## Notes`n" -Append
+                Out-File -FilePath $DocumentationFile -InputObject "$((GetStringBetweenTwoStrings -firstString '.NOTES' -secondString '#>' -importPath $File.FullName).trim())" -Append 
+
+                #TODO: Add overview page for all functions in documentation folder
+                Out-File -FilePath $OverviewFile -InputObject "* [$($GetHelp.Name)](Functions/$($GetHelp.Name).md)" -Append
+            }
+        }
+    }
+}
+
 ##############
 ### SCRIPT ###
 ##############
 Import-FunctionsToPSMFiles
+GenerateFunctionsDocumentation
 New-ModuleInstaller -Version $Version -ProductName $ProductName -UpgradeCode $UpgradeCode
 New-ModuleInstallerSDKOnly -Version $Version -ProductName $ProductName -UpgradeCode $UpgradeCode
 New-ModuleInstallerPowerPackOnly -Version $Version -ProductName $ProductName -UpgradeCode $UpgradeCode

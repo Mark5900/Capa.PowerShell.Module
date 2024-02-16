447f7e0d01aec2d8000fa2171425a7202232d6d1
Mark B. Rasmussen <mark@boennelykke.com>
2024-02-16 07:15:03 +0100

    fix: Added more error handling to Publish.ps1

--- Publish.ps1
+++ Publish.ps1
@@ -1,10 +1,25 @@
 $ModulesPath = Join-Path $PSScriptRoot 'Modules'
 $Modules = Get-ChildItem -Path $ModulesPath -Directory | Sort-Object { $_.Name.Length } -Descending
+
+$VersionPath = Join-Path $PSScriptRoot 'version.txt'
+$Version = (Get-Content -Path $VersionPath).Trim()
+
 foreach ($Module in $Modules) {
 	$ModulePath = Join-Path $ModulesPath $Module.Name 'Prod'
+
 	try {
 		Publish-Module -Path $ModulePath -NuGetApiKey $env:APIKEY
 	} catch {
 		Publish-PSResource -Path $ModulePath -ApiKey $env:APIKEY
+	}
+
+	$Run = $true
+
+	while ($Run) {
+		if (Find-Module -Name $Module.Name -RequiredVersion $Version -ErrorAction SilentlyContinue) {
+			$Run = $false
+		} else {
+			Start-Sleep -Seconds 1
+		}
 	}
 }

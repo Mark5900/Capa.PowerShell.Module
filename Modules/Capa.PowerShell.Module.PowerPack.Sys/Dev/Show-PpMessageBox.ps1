ff92cf23ff3bd6ae3e24a069afdcbf16f982db90
Mark B. Rasmussen <mark@boennelykke.com>
2024-04-23 22:18:50 +0200

    feat: Show-PpMessageBox

---
+++ Modules/Capa.PowerShell.Module.PowerPack.Sys/Dev/Show-PpMessageBox.ps1
@@ -0,0 +1,22 @@
+function Show-PpMessageBox {
+	[CmdletBinding()]
+	param (
+		[Parameter(Mandatory = $true)]
+		[string]$Title,
+		[Parameter(Mandatory = $true)]
+		[string]$Message,
+		[Parameter(Mandatory = $false)]
+		[ValidateSet('OK', 'OKCancel', 'YesNo', 'YesNoCancel')]
+		[string]$Buttons = 'OK',
+		[Parameter(Mandatory = $true)]
+		[string]$DefaultButton,
+		[Parameter(Mandatory = $false)]
+		[ValidateSet('Information', 'Warning', 'Error', 'Question')]
+		[string]$Icon = 'Information',
+		[Parameter(Mandatory = $false)]
+		[int]$TimeoutSeconds = 30,
+		[Parameter(Mandatory = $false)]
+		[bool]$Async = $false
+	)
+	return $Global:InputObject.ShowMessageBox($Title, $Message, $Buttons, $DefaultButton, $Icon, $TimeoutSeconds, $Async)
+}

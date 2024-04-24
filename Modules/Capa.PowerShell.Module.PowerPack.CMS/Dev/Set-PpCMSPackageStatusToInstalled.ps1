1c2eb93855832d6a6cd45e1522c2e84b76d64a1d
Mark B. Rasmussen <mark@boennelykke.com>
2024-04-24 20:21:57 +0200

    feat: Added function Set-PpCMSPackageStatusToInstalled

---
+++ Modules/Capa.PowerShell.Module.PowerPack.CMS/Dev/Set-PpCMSPackageStatusToInstalled.ps1
@@ -0,0 +1,31 @@
+# TODO: #345 Update Get-Help for Set-PpCMSPackageStatusToInstalled
+<#
+	.SYNOPSIS
+		Sets the status of the specified package to installed.
+
+	.DESCRIPTION
+		Sets the status of the specified package to installed.
+
+	.PARAMETER PackageName
+		The name of the package to set the status for.
+
+	.PARAMETER PackageVersion
+		The version of the package to set the status for.
+
+	.EXAMPLE
+		$bStatus = Set-PpCMSPackageStatusToInstalled -PackageName "MyPackage" -PackageVersion "1.0.0"
+		if ($bStatus) {
+			Job_WriteLog -Text "Package status set to installed."
+		} else {
+			Job_WriteLog -Text "Failed to set package status to installed."
+		}
+#>
+function Set-PpCMSPackageStatusToInstalled {
+	param (
+		[Parameter(Mandatory = $true)]
+		[string]$PackageName,
+		[Parameter(Mandatory = $true)]
+		[string]$PackageVersion
+	)
+	return CMS_SetPackageStatusToInstalled -package $PackageName -version $PackageVersion
+}

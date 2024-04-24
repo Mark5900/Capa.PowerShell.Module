457f41bf8e2f85c90d4683e4cf91db6cd3c018dc
Mark B. Rasmussen <mark@boennelykke.com>
2024-04-24 18:27:44 +0200

    feat: Added function Remove-PpCMSComputerFromCalendarGroup

---
+++ Modules/Capa.PowerShell.Module.PowerPack.CMS/Dev/Remove-PpCMSComputerFromCalendarGroup.ps1
@@ -0,0 +1,26 @@
+# TODO: #315 Update Get-Help for Remove-PpCMSComputerFromCalendarGroup
+<#
+	.SYNOPSIS
+		Removes a specified computer unit from a calendar group.
+
+	.DESCRIPTION
+		Removes a specified computer unit from a calendar group.
+
+	.PARAMETER Group
+		The name of the calendar group from which the computer unit should be removed.
+
+	.EXAMPLE
+		$bStatus = Remove-PpCMSComputerFromCalendarGroup -Group "MyCalendarGroup"
+		if ($bStatus) {
+			Job_WriteLog -Text "Computer unit removed from calendar group."
+		} else {
+			Job_WriteLog -Text "Failed to remove computer unit from calendar group."
+		}
+#>
+function Remove-PpCMSComputerFromCalendarGroup {
+	param (
+		[Parameter(Mandatory = $true)]
+		[string]$Group
+	)
+	return CMS_RemoveComputerFromCalendarGroup -group $Group
+}

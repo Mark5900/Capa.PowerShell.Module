1ebdf94b0cebc13a8bd19b42cf7e28171f8915a2
Mark B. Rasmussen <mark@boennelykke.com>
2024-04-24 14:49:02 +0200

    feat: Added function Uninstall-PpCMSPackageFromUnit

---
+++ Modules/Capa.PowerShell.Module.PowerPack.CMS/Dev/Uninstall-PpCMSPackageFromUnit.ps1
@@ -0,0 +1,48 @@
+# TODO: #307 Update Get-Help for Uninstall-PpCMSPackageFromUnit
+<#
+	.SYNOPSIS
+		Uninstalls package from unit.
+
+	.DESCRIPTION
+		Uninstalls package from unit.
+
+	.PARAMETER PackageID
+		ID of the package (JobId from the JOB table).
+
+	.PARAMETER PackageName
+		Name of the package.
+
+	.PARAMETER PackageVersion
+		Version of the package.
+
+	.EXAMPLE
+		$bStatus = Uninstall-PpCMSPackageFromUnit -PackageID 1234
+		if ($bStatus) {
+			Write-Host "Package uninstalled"
+		} else {
+			Write-Host "Failed to uninstall package"
+		}
+
+	.EXAMPLE
+		$bStatus = Uninstall-PpCMSPackageFromUnit -PackageName 'MyPackage' -PackageVersion 'v1.0'
+		if ($bStatus) {
+			Write-Host "Package uninstalled"
+		} else {
+			Write-Host "Failed to uninstall package"
+		}
+#>
+function Uninstall-PpCMSPackageFromUnit {
+	param (
+		[Parameter(ParameterSetName = 'ID', Mandatory = $true)]
+		[string]$PackageID,
+		[Parameter(ParameterSetName = 'NameVersion', Mandatory = $true)]
+		[string]$PackageName,
+		[Parameter(ParameterSetName = 'NameVersion', Mandatory = $true)]
+		[string]$PackageVersion
+	)
+	if ($PackageID) {
+		return CMS_UninstallPackageFromUnitByID -packageid $PackageID
+	} else {
+		return CMS_UninstallPackageFromUnit -package $PackageName -version $PackageVersion
+	}
+}

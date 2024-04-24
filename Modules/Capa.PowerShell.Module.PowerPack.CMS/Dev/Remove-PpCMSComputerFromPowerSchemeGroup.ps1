d98a67a699b13b29abd543c5d20c02b1b110faf5
Mark B. Rasmussen <mark@boennelykke.com>
2024-04-24 19:02:49 +0200

    feat: Added function Remove-PpCMSComputerFromPowerSchemeGroup

---
+++ Modules/Capa.PowerShell.Module.PowerPack.CMS/Dev/Remove-PpCMSComputerFromPowerSchemeGroup.ps1
@@ -0,0 +1,26 @@
+# TODO: #323 Update Get-Help for Remove-PpCMSComputerFromPowerSchemeGroup
+<#
+	.SYNOPSIS
+		Remove a computer from a Power Scheme Group.
+
+	.DESCRIPTION
+		Remove a computer from a Power Scheme Group.
+
+	.PARAMETER Group
+		The name of the Power Scheme Group.
+
+	.EXAMPLE
+		$bStatus = Remove-PpCMSComputerFromPowerSchemeGroup -Group "MyGroup"
+		if ($bStatus) {
+			Job_WriteLog -Text "Computer removed from Power Scheme Group."
+		} else {
+			Job_WriteLog -Text "Failed to remove computer from Power Scheme Group."
+		}
+#>
+function Remove-PpCMSComputerFromPowerSchemeGroup {
+	param (
+		[Parameter(Mandatory = $true)]
+		[string]$Group
+	)
+	return CMS_RemoveComputerFromPowerSchemeGroup -group $Group
+}

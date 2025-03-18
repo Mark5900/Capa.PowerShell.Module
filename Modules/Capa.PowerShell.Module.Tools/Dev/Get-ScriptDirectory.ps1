39756ed62252697befda34b87dc7fd71564c66f5
Mark B. Rasmussen <56847489+Mark5900@users.noreply.github.com>
2025-03-18 14:49:55 +0100

    fix: Get-ScriptDirectory does not work in when running a script

--- Modules/Capa.PowerShell.Module.Tools/Dev/Get-ScriptDirectory.ps1
+++ Modules/Capa.PowerShell.Module.Tools/Dev/Get-ScriptDirectory.ps1
@@ -6,10 +6,18 @@
 		Get the directory of the current script. It is better than $PSScriptRoot because it works also when running the script line by line in the console.
 #>
 Function Get-ScriptDirectory {
-	#neccessary so different powershell editors can be used
-	Switch ($Host.name) {
-		'Visual Studio Code Host' { Split-Path $psEditor.GetEditorContext().CurrentFile.Path }
-		'Windows PowerShell ISE Host' { Split-Path -Path $psISE.CurrentFile.FullPath }
-		'ConsoleHost' { $PSScriptRoot }
+	# Necessary so different PowerShell editors and module contexts can be used
+	if ($MyInvocation.ScriptName) {
+		# If called from a script, use the script's path
+		return Split-Path -Path $MyInvocation.ScriptName
+	} elseif ($Host.Name -eq 'Visual Studio Code Host') {
+		# If called from VS Code
+		return Split-Path $psEditor.GetEditorContext().CurrentFile.Path
+	} elseif ($Host.Name -eq 'Windows PowerShell ISE Host') {
+		# If called from PowerShell ISE
+		return Split-Path -Path $psISE.CurrentFile.FullPath
+	} else {
+		# Default to $PSScriptRoot (module or other contexts)
+		return $PSScriptRoot
 	}
 }

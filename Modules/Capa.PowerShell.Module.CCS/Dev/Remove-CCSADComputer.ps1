c90363dc25014fcb22b3e2b2135abe1ea94fdda0
Mark B. Rasmussen <56847489+Mark5900@users.noreply.github.com>
2025-03-24 12:15:17 +0100

    Update Remove-CCSADComputer.ps1

--- Modules/Capa.PowerShell.Module.CCS/Dev/Remove-CCSADComputer.ps1
+++ Modules/Capa.PowerShell.Module.CCS/Dev/Remove-CCSADComputer.ps1
@@ -1,3 +1,37 @@
+<#
+	.SYNOPSIS
+		Remove a computer from Active Directory using the CCS Web Service.
+
+	.DESCRIPTION
+		Removes a computer from Active Directory using the CCS Web Service. This function requires the CCS Web Service URL and credentials to access it.
+
+	.PARAMETER ComputerName
+		The name of the computer to be removed from Active Directory.
+
+	.PARAMETER DomainOUPath
+		The Organizational Unit (OU) path in which the computer resides.
+
+	.PARAMETER Domain
+		The domain in which the computer resides.
+
+	.PARAMETER Url
+		The URL of the CCS Web Service. Example "https://example.com/CCSWebservice/CCS.asmx".
+
+	.PARAMETER CCSCredential
+		The credentials used to authenticate with the CCS Web Service.
+
+	.PARAMETER DomainCredential
+		The credentials of an account with permissions to remove the computer from Active Directory, if not defined it will run in the CCSWebservice context.
+
+	.PARAMETER PasswordIsEncrypted
+		Indicates if the password in the DomainCredential is encrypted. Default is false.
+
+	.EXAMPLE
+		PS C:\> Remove-CCSADComputer -ComputerName "TestPC" -DomainOUPath "OU=Computers,DC=example,DC=com" -Domain "example.com" -Url "https://example.com/CCSWebservice/CCS.asmx" -CCSCredential $Credential
+
+	.EXAMPLE
+		PS C:\> Remove-CCSADComputer -ComputerName "TestPC" -DomainOUPath "OU=Computers,DC=example,DC=com" -Domain "example.com" -Url "https://example.com/CCSWebservice/CCS.asmx" -CCSCredential $Credential -DomainCredential $DomainCredential
+#>
 function Remove-CCSADComputer {
 	param (
 		[Parameter(Mandatory = $true)]

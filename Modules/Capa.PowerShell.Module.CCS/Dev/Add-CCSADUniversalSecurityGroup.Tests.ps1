de42e567a5af1a2b4f97ce8f5b75605eb22e205a
Mark B. Rasmussen <56847489+Mark5900@users.noreply.github.com>
2025-03-31 13:54:53 +0200

    feat: Added Add-CCSADUniversalSecurityGroup

--- Modules/Capa.PowerShell.Module.CCS/Dev/Add-CCSADUniversalSecurityGroup.Tests.ps1
+++ Modules/Capa.PowerShell.Module.CCS/Dev/Add-CCSADUniversalSecurityGroup.Tests.ps1
@@ -1,14 +1,14 @@
-# TODO: #434 Create Tests for Add-CCSADGlobalSecurityGroup
+# TODO: #435 Create Tests for Add-CCSADUniversalSecurityGroup
 
 $CCSCredential = Get-Credential -Message 'Enter CCS Web Service credentials'
 $DomainCredential = Get-Credential -Message 'Enter Domain credentials'
 
 $Splat = @{
-	GroupName        = 'Test_Add-CCSADGlobalSecurityGroup'
+	GroupName        = 'Test_Add-CCSADUniversalSecurityGroup'
 	Description      = 'Test Description'
 	Domain           = 'Firmax.local'
 	Url              = 'https://mracapa02.capainstaller.com/CCSWebservice/CCS.asmx'
 	CCSCredential    = $CCSCredential
 	DomainCredential = $DomainCredential
 }
-Add-CCSADGlobalSecurityGroup @Splat
+Add-CCSADUniversalSecurityGroup @Splat

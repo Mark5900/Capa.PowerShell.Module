3b477ba1a0d918f816163c323f0bf180a0512da8
Mark B. Rasmussen <56847489+Mark5900@users.noreply.github.com>
2025-07-16 14:33:54 +0200

    fix: Auto release no longer working

--- Publish.ps1
+++ Publish.ps1
@@ -1,29 +1,32 @@
+Import-Module Microsoft.PowerShell.PSResourceGet
+
 $ModulesPath = Join-Path $PSScriptRoot 'Modules'
 $Modules = Get-ChildItem -Path $ModulesPath -Directory | Sort-Object { $_.Name.Length } -Descending
 
 $VersionPath = Join-Path $PSScriptRoot 'version.txt'
 $Version = (Get-Content -Path $VersionPath).Trim()
 if ($Version -like '*-*') {
-	$FullVersion = $Version
-	$Version = $FullVersion.Split('-')[0]
-	$PrereleaseVersion = ($FullVersion -split '-', 2)[1]
-	$PrereleaseVersion = $PrereleaseVersion.Replace('.', '')
-	$Version = "$Version-$PrereleaseVersion"
+  $FullVersion = $Version
+  $Version = $FullVersion.Split('-')[0]
+  $PrereleaseVersion = ($FullVersion -split '-', 2)[1]
+  $PrereleaseVersion = $PrereleaseVersion.Replace('.', '')
+  $Version = "$Version-$PrereleaseVersion"
 }
 Write-Host "Publishing version $Version"
 
 foreach ($Module in $Modules) {
-	Write-Host "Publishing module $($Module.Name)"
-	$ModulePath = Join-Path $ModulesPath $Module.Name 'Prod'
-	$PsdPath = Join-Path $ModulePath "$($Module.Name).psd1"
+  Write-Host "Publishing module $($Module.Name)"
+  $ModulePath = Join-Path $ModulesPath $Module.Name 'Prod'
+  $PsdPath = Join-Path $ModulePath "$($Module.Name).psd1"
 
-	if (Find-Module -Name $Module.Name -RequiredVersion $Version -ErrorAction SilentlyContinue -AllowPrerelease) {
-		Write-Host "Module $($Module.Name) version $Version already published. Skipping" -ForegroundColor Green
-		continue
-	}
+  $Exist = Find-PSResource -Name $Module.Name -Type Module -Version $Version -Repository 'PSGallery' -Prerelease -ErrorAction SilentlyContinue
+  if ($Exist) {
+    Write-Host "Module $($Module.Name) version $Version already published. Skipping" -ForegroundColor Green
+    continue
+  }
 
-	try {
-		Publish-PSResource -Path $ModulePath -ApiKey $env:APIKEY -SkipDependenciesCheck -SkipModuleManifestValidate
+  try {
+    Publish-PSResource -Path $ModulePath -ApiKey $env:APIKEY -SkipDependenciesCheck -SkipModuleManifestValidate -Repository 'PSGallery'
 	} catch {
 		Write-Host "Failed to publish module $($Module.Name)" -ForegroundColor Red
 		Write-Host $_.Exception.Message

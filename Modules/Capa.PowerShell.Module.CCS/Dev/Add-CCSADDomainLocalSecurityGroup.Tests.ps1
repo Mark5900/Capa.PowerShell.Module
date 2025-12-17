86c9d374600dfec041e7274491fc59e1cb62b874
Mark B. Rasmussen <56847489+Mark5900@users.noreply.github.com>
2025-12-17 14:12:18 +0100

    Update unittests

--- Modules/Capa.PowerShell.Module.CCS/Dev/Add-CCSADDomainLocalSecurityGroup.Tests.ps1
+++ Modules/Capa.PowerShell.Module.CCS/Dev/Add-CCSADDomainLocalSecurityGroup.Tests.ps1
@@ -1,10 +1,1 @@
 # TODO: #432 Writes tests for Add-CCSADDomainLocalSecurityGroup
-$Splat = @{
-	GroupName        = 'TestCCSCreate'
-	Description      = 'Test Description'
-	Domain           = 'Firmax.local'
-	Url              = 'https://mracapa02.capainstaller.com/CCSWebservice/CCS.asmx'
-	CCSCredential    = $CCSCredential
-	DomainCredential = $DomainCredential
-}
-Add-CCSADDomainLocalSecurityGroup @Splat
--- Modules/Capa.PowerShell.Module.CCS/Dev/Get-CCSADComputerNames.Tests.ps1
+++
@@ -1,7 +0,0 @@
-$Splat = @{
-	Domain 				 = 'Firmax.local'
-	CCSCredential = $CCSCredential
-	Url          = 'https://mracapa02.capainstaller.com/CCSWebservice/CCS.asmx'
-	DomainCredential = $DomainCredential
-}
-$Result = Get-CCSADComputerNames @Splat

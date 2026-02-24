a1197729d867b6ca85ecbd3c49b80dd6ec54abbc
Mark B. Rasmussen <56847489+Mark5900@users.noreply.github.com>
2026-02-24 10:28:10 +0100

    fix: #209

--- Modules/Capa.PowerShell.Module.SDK.Unit/Dev/Get-CapaUnitDescription.ps1
+++ Modules/Capa.PowerShell.Module.SDK.Unit/Dev/Get-CapaUnitDescription.ps1
@@ -1,29 +1,47 @@
-# TODO: #209 Update and add tests
-
 <#
 	.SYNOPSIS
-		https://capasystems.atlassian.net/wiki/spaces/CI64DOC/pages/19306247474/Get+unit+description
+		Gets description for a unit.
 
 	.DESCRIPTION
-		A detailed description of the Get-CapaUnitDescription function.
+		Gets the description for the specified unit by calling the CapaSDK method
+		GetUnitDescription.
+
+	.PARAMETER CapaSDK
+		The initialized CapaSDK instance from Initialize-CapaSDK.
+
+	.PARAMETER UnitName
+		Name of the unit to query description for.
+
+	.PARAMETER UnitType
+		Type of unit. Valid values are Computer and User.
 
 	.EXAMPLE
-				PS C:\> Get-CapaUnitDescription
+		PS C:\> Get-CapaUnitDescription -CapaSDK $CapaSDK -UnitName 'PC-01' -UnitType Computer
+
+		Returns the description for PC-01.
 
 	.NOTES
-		Additional information about the function.
+		For more information, see:
+		https://capasystems.atlassian.net/wiki/spaces/CI64DOC/pages/19306247474/Get+unit+description
 #>
 function Get-CapaUnitDescription {
 	[CmdletBinding()]
+	[OutputType([object])]
 	param (
 		[Parameter(Mandatory = $true)]
-		$CapaSDK,
+		[ValidateNotNull()]
+		[pscustomobject]$CapaSDK,
 		[Parameter(Mandatory = $true)]
-		$UnitName,
+		[ValidateNotNullOrEmpty()]
+		[string]$UnitName,
 		[Parameter(Mandatory = $true)]
 		[ValidateSet('Computer', 'User')]
-		$UnitType
+		[string]$UnitType
 	)
+
+	if (-not ($CapaSDK.PSObject.Methods.Name -contains 'GetUnitDescription')) {
+		throw 'CapaSDK does not contain method GetUnitDescription.'
+	}
 
 	$value = $CapaSDK.GetUnitDescription($UnitName, $UnitType)
 	return $value

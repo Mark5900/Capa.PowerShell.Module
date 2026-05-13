8cc39e899aab19d2eab6892f3f295bb4276cbe21
Mark B. Rasmussen <56847489+Mark5900@users.noreply.github.com>
2026-05-13 12:56:27 +0200

    fix #330

--- Modules/Capa.PowerShell.Module.PowerPack.CMS/Dev/Add-PpCMSPackageToUnit.Tests.ps1
+++ Modules/Capa.PowerShell.Module.PowerPack.CMS/Dev/Add-PpCMSPackageToUnit.Tests.ps1
@@ -1,1 +1,11 @@
-# TODO: #330 Create tests for Add-PpCMSPackageToUnit
+BeforeAll {
+	. $PSCommandPath.Replace('.Tests.ps1', '.ps1')
+}
+
+Describe 'Add-PpCMSPackageToUnit' {
+	It 'Has CmdletBinding attribute' {
+		$command = Get-Command -Name 'Add-PpCMSPackageToUnit' -CommandType Function -ErrorAction Stop
+
+		($command.ScriptBlock.Attributes | Where-Object { $_ -is [System.Management.Automation.CmdletBindingAttribute] }) | Should -Not -BeNullOrEmpty
+	}
+}

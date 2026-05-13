e99010aaf2692f0c70008f5dbdc30d98d1c3a3de
Mark B. Rasmussen <56847489+Mark5900@users.noreply.github.com>
2026-05-13 12:58:33 +0200

    fix #292

--- Modules/Capa.PowerShell.Module.PowerPack.CMS/Dev/Get-PpCMSDeploymentTemplateVariable.Tests.ps1
+++ Modules/Capa.PowerShell.Module.PowerPack.CMS/Dev/Get-PpCMSDeploymentTemplateVariable.Tests.ps1
@@ -1,1 +1,11 @@
-# TODO: #292 Write tests for Get-PpCMSDeploymentTemplateVariable
+BeforeAll {
+	. $PSCommandPath.Replace('.Tests.ps1', '.ps1')
+}
+
+Describe 'Get-PpCMSDeploymentTemplateVariable' {
+	It 'Has CmdletBinding attribute' {
+		$command = Get-Command -Name 'Get-PpCMSDeploymentTemplateVariable' -CommandType Function -ErrorAction Stop
+
+		($command.ScriptBlock.Attributes | Where-Object { $_ -is [System.Management.Automation.CmdletBindingAttribute] }) | Should -Not -BeNullOrEmpty
+	}
+}

1af11a5425feebc5f7bedbf3737b372690f016ff
Mark B. Rasmussen <56847489+Mark5900@users.noreply.github.com>
2026-05-13 13:05:07 +0200

    fix #289

--- Modules/Capa.PowerShell.Module.PowerPack.CMS/Dev/Get-PpCmsProperty.Tests.ps1
+++ Modules/Capa.PowerShell.Module.PowerPack.CMS/Dev/Get-PpCmsProperty.Tests.ps1
@@ -1,1 +1,11 @@
-# TODO: #289 Create tests for Get-PpCmsProperty
+BeforeAll {
+	. $PSCommandPath.Replace('.Tests.ps1', '.ps1')
+}
+
+Describe 'Get-PpCMSProperty' {
+	It 'Has CmdletBinding attribute' {
+		$command = Get-Command -Name 'Get-PpCMSProperty' -CommandType Function -ErrorAction Stop
+
+		($command.ScriptBlock.Attributes | Where-Object { $_ -is [System.Management.Automation.CmdletBindingAttribute] }) | Should -Not -BeNullOrEmpty
+	}
+}

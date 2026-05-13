7bef1e58ce279c1f59b2cdbae47e5ea7688cc414
Mark B. Rasmussen <56847489+Mark5900@users.noreply.github.com>
2026-05-13 13:25:20 +0200

    fix #380, fix #391, fix #383, fix #506, fix #392, fix #384

--- Modules/Capa.PowerShell.Module.PowerPack.UsrMgr/Dev/UsrMgr_ExistLocalUserAccount.Tests.ps1
+++ Modules/Capa.PowerShell.Module.PowerPack.UsrMgr/Dev/UsrMgr_ExistLocalUserAccount.Tests.ps1
@@ -1,1 +1,28 @@
-# TODO: #383 Create tests for UsrMgr_ExistLocalUserAccountS
+BeforeAll {
+	. $PSCommandPath.Replace('.Tests.ps1', '.ps1')
+}
+
+Describe 'UsrMgr_ExistLocalUserAccount' {
+	BeforeEach {
+		$script:capturedUserName = $null
+		$global:cs = [pscustomobject]@{}
+		$global:cs | Add-Member ScriptMethod UsrMgr_ExistLocalUserAccount {
+			param($UserName)
+			$script:capturedUserName = $UserName
+			$true
+		}
+	}
+
+	It 'Calls the cs method and returns the result' {
+		$result = UsrMgr_ExistLocalUserAccount -UserName 'JohnDoe'
+
+		$result | Should -Be $true
+		$script:capturedUserName | Should -Be 'JohnDoe'
+	}
+
+	It 'Has CmdletBinding attribute' {
+		$command = Get-Command -Name 'UsrMgr_ExistLocalUserAccount' -CommandType Function -ErrorAction Stop
+
+		($command.ScriptBlock.Attributes | Where-Object { $_ -is [System.Management.Automation.CmdletBindingAttribute] }) | Should -Not -BeNullOrEmpty
+	}
+}
--- Modules/Capa.PowerShell.Module.PowerPack.UsrMgr/Dev/UsrMgr_RemoveUserFromLocalGroup.Tests.ps1
+++ Modules/Capa.PowerShell.Module.PowerPack.UsrMgr/Dev/UsrMgr_RemoveUserFromLocalGroup.Tests.ps1
@@ -1,1 +1,28 @@
-# TODO: #384 Create tests for UsrMgr_RemoveUserFromLocalGroup
+BeforeAll {
+	. $PSCommandPath.Replace('.Tests.ps1', '.ps1')
+}
+
+Describe 'UsrMgr_RemoveUserFromLocalGroup' {
+	BeforeEach {
+		$script:capturedArgs = @()
+		$global:cs = [pscustomobject]@{}
+		$global:cs | Add-Member ScriptMethod UsrMgr_RemoveUserFromLocalGroup {
+			param($Domain, $UserName, $GroupName)
+			$script:capturedArgs = @($Domain, $UserName, $GroupName)
+		}
+	}
+
+	It 'Calls the cs method with the provided domain, user and group' {
+		UsrMgr_RemoveUserFromLocalGroup -Domain 'CONTOSO' -UserName 'JohnDoe' -GroupName 'Administrators'
+
+		$script:capturedArgs[0] | Should -Be 'CONTOSO'
+		$script:capturedArgs[1] | Should -Be 'JohnDoe'
+		$script:capturedArgs[2] | Should -Be 'Administrators'
+	}
+
+	It 'Has CmdletBinding attribute' {
+		$command = Get-Command -Name 'UsrMgr_RemoveUserFromLocalGroup' -CommandType Function -ErrorAction Stop
+
+		($command.ScriptBlock.Attributes | Where-Object { $_ -is [System.Management.Automation.CmdletBindingAttribute] }) | Should -Not -BeNullOrEmpty
+	}
+}

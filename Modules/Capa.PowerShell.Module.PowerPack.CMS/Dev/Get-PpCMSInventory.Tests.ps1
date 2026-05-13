4686453de28d98bf56166614a38a5278c0849135
Mark B. Rasmussen <56847489+Mark5900@users.noreply.github.com>
2026-05-13 13:01:06 +0200

    fix #294

--- Modules/Capa.PowerShell.Module.PowerPack.CMS/Dev/Get-PpCMSInventory.Tests.ps1
+++ Modules/Capa.PowerShell.Module.PowerPack.CMS/Dev/Get-PpCMSInventory.Tests.ps1
@@ -1,1 +1,11 @@
-# TODO: #294 Write tests for Get-PpCMSInventory
+BeforeAll {
+	. $PSCommandPath.Replace('.Tests.ps1', '.ps1')
+}
+
+Describe 'Get-PpCMSInventory' {
+	It 'Has CmdletBinding attribute' {
+		$command = Get-Command -Name 'Get-PpCMSInventory' -CommandType Function -ErrorAction Stop
+
+		($command.ScriptBlock.Attributes | Where-Object { $_ -is [System.Management.Automation.CmdletBindingAttribute] }) | Should -Not -BeNullOrEmpty
+	}
+}
